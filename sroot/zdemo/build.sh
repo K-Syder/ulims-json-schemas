7fe432f9b5154fae4d2b250feb330ec03d260e8b
Comina, Martin (DLSLtd,RAL,LSCI) <martin.comina@diamond.ac.uk>
2025-03-17 17:30:47 +0000

    Add pattern 9 to zdemo schema build.

--- sroot/zdemo/build.sh
+++ sroot/zdemo/build.sh
@@ -19,3 +19,13 @@ npm run build-new sroot/zdemo/pattern1/child/three/current.yaml
 npm run build-new sroot/zdemo/pattern1/child/two/current.yaml 
 npm run build-new sroot/zdemo/pattern1/child/one/current.yaml 
 npm run build-new sroot/zdemo/pattern1/top/current.yaml 
+
+
+# Build pattern 9 schemas.
+#
+# Children moving at faster or slower paces with top
+# schema having point releases at fast or slow pace.
+npm run build-new sroot/zdemo/pattern9/faster/current.yaml
+npm run build-new sroot/zdemo/pattern9/slower/current.yaml
+npm run build-new sroot/zdemo/pattern9/topfast/current.yaml
+npm run build-new sroot/zdemo/pattern9/topslow/current.yaml
